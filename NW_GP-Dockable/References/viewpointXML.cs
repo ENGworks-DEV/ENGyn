28b35cd8e5da643c5378e26cbc7deb03b3ec9893
Pablo Derendinger <pderendinger@gmail.com>
2018-09-01 11:44:58 -0300

    Classes for xml searchset files

--- NW_GP-Dockable/References/viewpointXML.cs
+++ NW_GP-Dockable/References/viewpointXML.cs
@@ -8,91 +8,19 @@ using System.Xml.Serialization;
 using System.Collections.Generic;
 namespace NW_GraphicPrograming.XML
 {
-    [XmlRoot(ElementName = "name")]
-    public class Name
-    {
-        [XmlAttribute(AttributeName = "internal")]
-        public string Internal { get; set; }
-        [XmlText]
-        public string Text { get; set; }
-    }
 
-    [XmlRoot(ElementName = "category")]
-    public class Category
-    {
-        [XmlElement(ElementName = "name")]
-        public Name Name { get; set; }
-    }
-
-    [XmlRoot(ElementName = "property")]
-    public class Property
-    {
-        [XmlElement(ElementName = "name")]
-        public Name Name { get; set; }
-    }
-
-    [XmlRoot(ElementName = "data")]
-    public class Data
-    {
-        [XmlElement(ElementName = "name")]
-        public Name Name { get; set; }
-        [XmlAttribute(AttributeName = "type")]
-        public string Type { get; set; }
-        [XmlText]
-        public string Text { get; set; }
-    }
-
-    [XmlRoot(ElementName = "value")]
-    public class Value
-    {
-        [XmlElement(ElementName = "data")]
-        public Data Data { get; set; }
-    }
-
-    [XmlRoot(ElementName = "condition")]
-    public class Condition
-    {
-        [XmlElement(ElementName = "category")]
-        public Category Category { get; set; }
-        [XmlElement(ElementName = "property")]
-        public Property Property { get; set; }
-        [XmlElement(ElementName = "value")]
-        public Value Value { get; set; }
-        [XmlAttribute(AttributeName = "test")]
-        public string Test { get; set; }
-        [XmlAttribute(AttributeName = "flags")]
-        public string Flags { get; set; }
-    }
-
-    [XmlRoot(ElementName = "conditions")]
-    public class Conditions
-    {
-        [XmlElement(ElementName = "condition")]
-        public List<Condition> Condition { get; set; }
-    }
-
-    [XmlRoot(ElementName = "findspec")]
-    public class Findspec
-    {
-        [XmlElement(ElementName = "conditions")]
-        public Conditions Conditions { get; set; }
-        [XmlElement(ElementName = "locator")]
-        public string Locator { get; set; }
-        [XmlAttribute(AttributeName = "mode")]
-        public string Mode { get; set; }
-        [XmlAttribute(AttributeName = "disjoint")]
-        public string Disjoint { get; set; }
-    }
 
     [XmlRoot(ElementName = "selectionset")]
     public class Selectionset
     {
-        [XmlElement(ElementName = "findspec")]
-        public Findspec Findspec { get; set; }
+        //[XmlElement(ElementName = "findspec")]
+        //public Findspec Findspec { get; set; }
         [XmlAttribute(AttributeName = "name")]
         public string Name { get; set; }
         [XmlAttribute(AttributeName = "guid")]
         public string Guid { get; set; }
+        [XmlAttribute(AttributeName = "color")]
+        public string color { get; set; }
     }
 
     [XmlRoot(ElementName = "viewfolder")]
@@ -104,6 +32,8 @@ namespace NW_GraphicPrograming.XML
         public string Name { get; set; }
         [XmlAttribute(AttributeName = "guid")]
         public string Guid { get; set; }
+        [XmlAttribute(AttributeName = "color")]
+        public string color { get; set; }
     }
 
     [XmlRoot(ElementName = "selectionsets")]
