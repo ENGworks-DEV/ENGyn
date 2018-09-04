7fa3ae17b61a29e334fae8b35f68f45d58ed8070
Pablo Derendinger <pderendinger@gmail.com>
2018-09-04 18:32:08 -0300

    GEtApiParameters

---
+++ Nodes/GetAPIParameters.cs
@@ -0,0 +1,113 @@
+﻿using System.Windows.Controls;
+using System.Xml;
+using Autodesk.Navisworks.Api;
+using TUM.CMS.VplControl.Nodes;
+using Autodesk.Navisworks.Api.Clash;
+using TUM.CMS.VplControl.Core;
+using System.Windows.Data;
+
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+using System.Windows;
+
+namespace NW_GraphicPrograming.Nodes
+{
+    public class NW_GetAPIParameter : Node
+    {
+        public NW_GetAPIParameter(VplControl hostCanvas)
+            : base(hostCanvas)
+        {
+            AddInputPortToNode("Input", typeof(object));
+            
+            AddOutputPortToNode("Output", typeof(object));
+
+            AddControlToNode(new Label() { Content = "Get API Parameters", FontSize = 13, FontWeight = FontWeights.Bold });
+
+
+        }
+
+
+        public override void Calculate()
+        {
+            List<object> output = new List<object>();
+            if (InputPorts[0].Data != null && InputPorts[1].Data != null)
+            {
+                if (InputPorts[0].Data.GetType() == typeof(List<object>))
+                {
+                    foreach (var item in InputPorts[0].Data as List<object>)
+                    {
+                        try
+                        {
+
+                            var properties = item.GetType().GetProperties();
+                            foreach (var p in properties)
+                            {
+                                output.Add(p.Name);
+                            }
+
+                        }
+
+                        catch
+                        {
+                            output.Add(null);
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    try
+                    {
+                        var item = InputPorts[0].Data;
+                        var properties = item.GetType().GetProperties();
+                        foreach (var p in properties)
+                        {
+                            output.Add(p.Name);
+                        }
+                    }
+                    catch
+                    {
+                        output.Add(null);
+                    }
+
+
+                }
+
+                OutputPorts[0].Data = output;
+            }
+                
+        }
+
+
+        public override void SerializeNetwork(XmlWriter xmlWriter)
+        {
+            base.SerializeNetwork(xmlWriter);
+
+            // add your xml serialization methods here
+        }
+
+        public override void DeserializeNetwork(XmlReader xmlReader)
+        {
+            base.DeserializeNetwork(xmlReader);
+
+            // add your xml deserialization methods here
+        }
+
+        public override Node Clone()
+        {
+            return new NW_GetAPIParameter(HostCanvas)
+            {
+                Top = Top,
+                Left = Left
+            };
+
+        }
+    }
+
+}
+namespace NW_GraphicPrograming
+{
+
+}
