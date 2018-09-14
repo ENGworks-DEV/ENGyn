814a87458f16fe18812ca18bf1b406588e2693de
Pablo Derendinger <pderendinger@gmail.com>
2018-09-14 12:29:59 -0300

    Assembly ENGworks

--- NW_GP-Dockable/Properties/AssemblyInfo.cs
+++ NW_GP-Dockable/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfDockPane")]
+[assembly: AssemblyTitle("ENGyn")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Autodesk, Inc.")]
-[assembly: AssemblyProduct("WpfDockPane")]
-[assembly: AssemblyCopyright("Copyright © Autodesk, Inc. 2010")]
+[assembly: AssemblyCompany("ENGworks")]
+[assembly: AssemblyProduct("ENGyn")]
+[assembly: AssemblyCopyright("Copyright © ENGworksInc. 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
