111c9664c9d0a91ca83254eb10ca4fe90bcb5730
Pabloderen <pderendinger@gmail.com>
2018-10-30 14:27:25 -0300

    ENGyn-fix: Mapping parameters

--- src/ENGyn-Nodes/API/GetAPIParameterValue.cs
+++ src/ENGyn-Nodes/API/GetAPIParameterValue.cs
@@ -10,6 +10,12 @@ using System.Collections.Generic;
 using System;
 using System.Reflection;
 using System.Windows;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
 
 namespace ENGyn.Nodes.API
 {
@@ -125,9 +131,19 @@ namespace ENGyn.Nodes.API
                             var types = iterator.GetType();
                             PropertyInfo prop = types.GetProperty(method);
 
-                            object value = prop.GetValue(iterator);
+                           
+                            if (MainTools.IsList(iterator))
+                            {
+                                Map(prop, (List<object>)iterator);
+                            }
 
-                            output.Add(value);
+                            else
+                            {
+                                object value = prop.GetValue(iterator);
+
+                                output.Add(value);
+                            }
+
                         }
 
                         catch
@@ -152,6 +168,27 @@ namespace ENGyn.Nodes.API
                 Left = Left
             };
 
+        }
+
+
+        /// <summary>
+        ///     Produces a new list by applying a projection function to each item of the input list(s) and
+        ///     storing the result.
+        /// </summary>
+        /// <param name="Property">
+        ///     Function that consumes an item from each input list and produces a value that is stored
+        ///     in the output list.
+        /// </param>
+        /// <param name="lists">Lists to be combined/mapped into a new list.</param>
+        public static IList<object> Map(PropertyInfo Property, params IEnumerable<object>[] lists)
+        {
+            if (!lists.Any())
+                throw new ArgumentException("Need at least one list to map.");
+            IEnumerable<List<object>> argList = lists[0].Select(x => new List<object> { x });
+            foreach (var pair in
+                lists.Skip(1).SelectMany(list => list.Zip(argList, (o, objects) => new { o, objects })))
+                pair.objects.Add(pair.o);
+            return argList.Select(x => Property.GetValue(x.ToArray())).ToList();
         }
     }
 
