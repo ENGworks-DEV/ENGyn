ad9df37ebf13758164dd7b02308576b68ee87393
Pabloderen <pderendinger@gmail.com>
2018-11-08 11:00:37 -0300

    ENGyn-fix: bat file to copy depen

---
+++ src/depencencies.bat
@@ -0,0 +1,14 @@
+cls
+echo. Coping Dependencies
+@echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+set myadress= %~dp0
+xcopy /Y %myadress%TUM.CMS.VPLControl\bin\TUM.CMS.VplControl.dll "C:\Program Files\Autodesk\Navisworks Manage 2018\Dependencies\"
+xcopy /Y  %myadress%TUM.CMS.VPLControl\bin\NCalc.dll "C:\Program Files\Autodesk\Navisworks Manage 2018\Dependencies\"
+xcopy /Y  %myadress%TUM.CMS.VPLControl\bin\Newtonsoft.Json.dll "C:\Program Files\Autodesk\Navisworks Manage 2018\Dependencies\"
+xcopy /Y  %myadress%TUM.CMS.VPLControl\bin\RadialMenu.dll "C:\Program Files\Autodesk\Navisworks Manage 2018\Dependencies\"
+xcopy /Y  %myadress%TUM.CMS.VPLControl\bin\TUM.CMS.VplControl.pdb "C:\Program Files\Autodesk\Navisworks Manage 2018\Dependencies\"
+xcopy /Y  %myadress%TUM.CMS.VPLControl\bin\Xceed.Wpf.Toolkit.dll "C:\Program Files\Autodesk\Navisworks Manage 2018\Dependencies\"
+echo FINISH
+set /p=Hit ENTER to continue...
+
